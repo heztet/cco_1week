1f1ae2cd4d405ad4dc5aeae2824d9c1aecb75600
Nicky Marino <nsmarino96@gmail.com>
2016-02-01 21:21:13 -0500

    Add student controller

---
+++ OneWeek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneWeek.MvcApplication" Language="C#" %>
---
+++ OneWeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneWeek.Startup))]
+namespace OneWeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
