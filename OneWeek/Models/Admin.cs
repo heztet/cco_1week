fff5f4f4c4969c724b7e321302c75e945d5dd7ac
Nicky Marino <nsmarino96@gmail.com>
2016-02-03 13:14:44 -0500

    Revert back from admin branch

---
+++ OneWeek/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OneWeek.Models
+{
+    public class Admin
+    {
+        public int AdminID { get; set; }
+        public string Name { get; set; }
+    }
+}
